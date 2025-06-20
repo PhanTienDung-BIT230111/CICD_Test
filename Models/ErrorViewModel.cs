f20e180148770fdc18eecf1a8fee839322cc6883
PhanTienDung-BIT230111 <phandung2k5@gmail.com>
2025-06-20 09:59:32 +0700

    v1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CICD2006.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
